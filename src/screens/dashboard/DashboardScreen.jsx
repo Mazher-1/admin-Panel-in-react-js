c2ef4c6ebd5f0d34d62117404cef42554a142f0c
MH Code <mazheriftikhar1@gmail.com>
2024-10-26 20:45:42 +0500

    26-oct changes

--- src/screens/dashboard/DashboardScreen.jsx
+++ src/screens/dashboard/DashboardScreen.jsx
@@ -1,10 +1,18 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
 import { AreaCards, AreaCharts, AreaTable, AreaTop } from "../../components";
 
 const Dashboard = () => {
+  const location = useLocation();
+  const { income = 0, monthlyExpenses = 0 } = location.state || {};
+
+  // Calculate total balance
+  const totalBalance = income - monthlyExpenses;
+
   return (
     <div className="content-area">
       <AreaTop />
-      <AreaCards />
+      <AreaCards income={income} expenses={monthlyExpenses} totalBalance={totalBalance} />
       <AreaCharts />
       <AreaTable />
     </div>
